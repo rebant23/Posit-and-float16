a1888225c1c406bd0049f06ad1ad6afe974f3544
Reyansh-B <rnbagaria@gmail.com>
2026-02-08 03:35:17 +0530

    Update NewFunctions.py

--- NewFunctions.py
+++ NewFunctions.py
@@ -1,11 +1,11 @@
 from arithmetics import basic_functions
 
-fn=basic_functions("float")
+fn=basic_functions("posit")
 
 # print(fn.mul(5.75,2.25))
 # print(fn.add(5.75,2.25))
 
-def mul(a,b,es=0):
+def mul(a,b,es=1):
     return fn.mul(a,b,es)
 
 def add(a,b):
@@ -43,10 +43,6 @@ def exp(a):
         + mul(power(a, 6), 0.001388888888888889)
         + mul(power(a, 7), 0.0001984126984126984)
         + mul(power(a, 8), 2.48015873015873e-05)
-        + mul(power(a, 9), 2.7557319223985893e-06)
-        + mul(power(a, 10), 2.755731922398589e-07)
-        + mul(power(a, 11), 2.505210838544172e-08)
-        + mul(power(a, 12), 2.08767569878681e-09)
-        + mul(power(a, 13), 1.6059043836821613e-10)
-        + mul(power(a, 14), 1.1470745597729725e-11)
+        
     )
+print(exp(3))
